19fa95c53affc4b7981c395d6592051272e5dbfe
lyj <kasean@nate.com>
2024-02-10 23:42:47 +0900

    batch_study

---
+++ src/test/java/com/example/spring_batchstudy/SpringBatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_batchstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
